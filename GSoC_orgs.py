1c94db279a1163f0054e0bf829c1dc874b4422cf
adhyay2000 <adityabundi2000@gmail.com>
2020-12-25 13:28:09 +0530

    Added progressbar and current year choice.

--- GSoC_orgs.py
+++ GSoC_orgs.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 from pandas import DataFrame
-
-url = 'https://summerofcode.withgoogle.com/archive/2018/organizations/'
+from progress.bar import Bar
+import sys
+year = 2018
+if len(sys.argv)==2:
+        year = sys.argv[1]
+url = 'https://summerofcode.withgoogle.com/archive/'+year+'/organizations/'
 
 r = requests.get(url)
 
@@ -22,25 +26,27 @@ techlist = []
 slots = []
 ideas = []
 
-for org_url in link_list:
-        lisat = []
-        r = requests.get(org_url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        org = soup.find('div', class_="banner__text")              
-        OrgName.append(f"=HYPERLINK(\"{org_url}\",\"{org.h3.text}\")")
-        technologies = soup.find_all('li', class_="organization__tag--technology")
-        for technology in technologies:
-                lisat.append(technology.text)
-        mys = ', '.join(lisat)
-        techlist.append(mys)
-        irc = soup.select_one(".org__meta-button")['href']
-        Contactlink.append(irc)
-        projects = soup.find('ul', class_="project-list-container")
-        slot = projects.findChildren('li')
-        slots.append(len(slot))
-        idea = soup.select_one(".org__button-container md-button")['href']
-        # idea = ideas[0].select_one('md-button')['href']
-        ideas.append(idea)
+with Bar('Processing...',max=len(link_list)) as bar:
+        for i,org_url in enumerate(link_list):
+                lisat = []
+                r = requests.get(org_url)
+                soup = BeautifulSoup(r.text, 'html.parser')
+                org = soup.find('div', class_="banner__text")              
+                OrgName.append(f"=HYPERLINK(\"{org_url}\",\"{org.h3.text}\")")
+                technologies = soup.find_all('li', class_="organization__tag--technology")
+                for technology in technologies:
+                        lisat.append(technology.text)
+                mys = ', '.join(lisat)
+                techlist.append(mys)
+                irc = soup.select_one(".org__meta-button")['href']
+                Contactlink.append(irc)
+                projects = soup.find('ul', class_="project-list-container")
+                slot = projects.findChildren('li')
+                slots.append(len(slot))
+                idea = soup.select_one(".org__button-container md-button")['href']
+                # idea = ideas[0].select_one('md-button')['href']
+                ideas.append(idea)
+                bar.next()
         
         
 
